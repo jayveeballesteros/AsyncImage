045dc82d726cc5d8e8fdc19ff8bbab2e0d60a4db
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-05-28 00:07:16 +0800

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Jayvee Ballesteros on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
